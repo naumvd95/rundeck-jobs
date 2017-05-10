ec5f88edad6218e8e2c4df80328a811324fa71b2
root <root@ci01.cicd-lab-dev.local>
2017-05-10 14:20:02 +0000

    sudo added

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-apt-get install -y build-essential libssl-dev libffi-dev python-dev libyaml-dev python-dev python-pip
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev libyaml-dev python-dev python-pip
 
-pip install virtualenv
-virtualenv venv-devops-portal-test
+sudo pip install virtualenv
+sudo virtualenv venv-devops-portal-test
 cd /var/rundeck/projects/cicd/scm/healthchecks
 source venv-devops-portal-test/bin/activate
 pip install -r requirements.txt
