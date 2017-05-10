01eea07c77bb7db88009f1e7c82235aa4efd62d5
root <root@ci01.cicd-lab-dev.local>
2017-05-10 14:23:35 +0000

    redefine opts

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -7,5 +7,5 @@ cd /var/rundeck/projects/cicd/scm/healthchecks
 source venv-devops-portal-test/bin/activate
 pip install -r requirements.txt
 pip install -r test-requirements.txt
-export CONFIG_FILE_PATH=$CONFIG_PATH
-pytest -m $HEALTHCHECK
+export CONFIG_FILE_PATH=$RD_OPTION_CONFIG_PATH
+pytest -m $RD_OPTION_HEALTHCHECK
