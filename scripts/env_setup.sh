67024540de531313bc12d9cd04627483b8d962d1
root <root@ci01.cicd-lab-dev.local>
2017-05-10 15:24:02 +0000

    sdfsdaf

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/bash 
+
+set -x
+
 sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev libyaml-dev python-dev python-pip
 
 sudo pip install virtualenv
-sudo virtualenv venv-devops-portal-test
 cd test-health/devops_portal_tests
+sudo virtualenv venv-devops-portal-test
 source venv-devops-portal-test/bin/activate
 pip install -r requirements.txt
 pip install -r test-requirements.txt
